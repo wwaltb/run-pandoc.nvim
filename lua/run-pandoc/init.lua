4da1d5b4cbff9b8ca09b94d870d506444c64a97a
walt <wwaltb@proton.me>
2024-05-10 01:21:12 -0700

    fixed error messages and added more known filetypes

--- lua/run-pandoc/init.lua
+++ lua/run-pandoc/init.lua
@@ -3,23 +3,32 @@ local M = {}
 function M.setup() end
 
 function M.compile()
-   local supported_filetypes = ["markdown", "rst", "html", "latex", "docx"]
-   local buf_file_type = vim.api.nvim_buf_get_option(0, "filetype")
-   if buf_file_type ~= "markdown" then
-      print("run_pandoc: Not a markdown file")
-      return
-   end
+	local supported_filetypes = { "markdown", "rst", "html", "latex", "docx", "org" }
+	local buf_filetype = vim.api.nvim_buf_get_option(0, "filetype")
 
-   local buf_name = vim.api.nvim_buf_get_name(0)
-   local buf_name_pdf_ext = buf_name:match("(.+)%..+") .. ".pdf"
+	local found = false
+	for k in pairs(supported_filetypes) do
+		if buf_filetype == supported_filetypes[k] then
+			found = true
+		end
+	end
 
-   local output = vim.fn.system(string.format("pandoc %s -o %s", buf_name, buf_name_pdf_ext))
+	if not found then
+		print("run_pandoc: Not a supported filetype")
+		return
+	end
 
-   if vim.v.shell_error == 0 then
-      print("run_pandoc: Compiled successfully to " .. buf_name_pdf_ext)
-   else
-      print("run_pandoc: Error - " .. output)
-   end
+	local buf_name = vim.api.nvim_buf_get_name(0)
+	local buf_name_pdf_ext = buf_name:match("(.+)%..+") .. ".pdf"
+
+	local output = vim.fn.system(string.format("pandoc %s -o %s 2>&1", buf_name, buf_name_pdf_ext))
+	print(output)
+
+	if vim.v.shell_error == 0 then
+		print("run_pandoc: Compiled successfully to " .. buf_name_pdf_ext)
+	else
+		vim.api.nvim_echo({ { output } }, true, {})
+	end
 end
 
 return M
